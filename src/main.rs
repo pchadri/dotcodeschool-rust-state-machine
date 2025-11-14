2d227292df00a7d2669773fd4cf802e4ec0e9fc1
Peter <pchadri@gmail.com>
2025-11-14 03:49:57 -0500

    Prepared to execute section 5 lesson 3 by copying the TODO from the course-follow-along to my main.rs which included the impl crate::support::Dispatch for Runtime, fn execute_block() and pub enum RuntimeCall. There seems to be an error due to ln75 where AccountID is used whereas an associated type already exists with the same name. THere may be a need to check if I should import crate::Runtime at the very beginning too.

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ mod types {
 }
 
 pub enum RuntimeCall {
-				
+				// TODO: Not implemented yet.	
 }
 
 #[derive(Debug)]
@@ -51,6 +51,40 @@ impl Runtime {
 	// Create a new instance of the main Runtime, by creating a new instance of each pallet.
 	fn new() -> Self {
 		Self { system: system::Pallet::<Self>::new(), balances: balances::Pallet::<Self>::new() }
+	}
+
+	// Execute a block of extrinsics. Increments the block number.
+	fn execute_block(&mut self, block: types::Block) -> support::DispatchResult {
+		/* TODO:
+			- Increment the system's block number.
+			- Check that the block number of the incoming block matches the current block number,
+			  or return an error.
+			- Iterate over the extrinsics in the block...
+				- Increment the nonce of the caller.
+				- Dispatch the extrinsic using the `caller` and the `call` contained in the extrinsic.
+				- Handle errors from `dispatch` same as we did for individual calls: printing any
+				  error and capturing the result.
+				- You can extend the error message to include information like the block number and
+				  extrinsic number.
+		*/
+		Ok(())
+	}
+}
+
+impl crate::support::Dispatch for Runtime {
+	type Caller = <Runtime as system::Config>::AccountId;
+	type Call = RuntimeCall;
+	// Dispatch a call on behalf of a caller. Increments the caller's nonce.
+	//
+	// Dispatch allows us to identify which underlying module call we want to execute.
+	// Note that we extract the `caller` from the extrinsic, and use that information
+	// to determine who we are executing the call on behalf of.
+	fn dispatch(
+		&mut self,
+		caller: Self::Caller,
+		runtime_call: Self::Call,
+	) -> support::DispatchResult {
+		unimplemented!();
 	}
 }
 
